29881aa7917aec515dfd0f221ec5dc4cd14f12a1
Efraim <efraim@pm.mt.gov.br>
2016-03-26 14:35:38 -0400

    Add URL for creating a new list via POST

--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
 	return render(request, 'home.html')
 
 def view_list(request):
 	return render(request, 'list.html', {'items': Item.objects.all()})
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
