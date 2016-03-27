315ca4887c178e68b22bfa7f8eb74002b7726c08
Efraim <efraim@pm.mt.gov.br>
2016-03-27 19:52:22 -0400

    Moved Item creation from views to forms

--- lists/forms.py
+++ lists/forms.py
@@ -5,6 +5,10 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
+
+	def save(self, for_list):
+		self.instance.list = for_list
+		return super().save()
 	
 	class Meta:
 		model = Item
